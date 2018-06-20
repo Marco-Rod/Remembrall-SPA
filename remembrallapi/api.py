3b98860e7858d1afad1385cf5211ca227f5f2396
Marco Antonio Rodriguez Torres <mrc5622@gmail.com>
2018-06-20 18:33:57 -0500

    add api plan pagination

--- remembrallapi/api.py
+++ remembrallapi/api.py
@@ -15,7 +15,7 @@ def fetch_users():
         obj = {}
         url = '/users'
         start = int(request.args.get('start', 1))
-        limit = int(request.args.get('limit', 20))
+        limit = int(request.args.get('limit', 10))
         users = User.query.all()
         count = len(users)
         if (count < start):
@@ -40,7 +40,7 @@ def fetch_users():
         return jsonify(obj)
 
     elif request.method == "POST":
-        data = request.get_json()
+        data = request.get_json(silent=True)
         user = User(
             name=data["name"],
             last_name=data["last_name"],
@@ -62,9 +62,46 @@ def user(id):
 @api.route("/plans", methods=["GET", "POST"])
 def fetch_plans():
     if request.method == "GET":
+        obj = {}
+        url = '/plans'
+        start = int(request.args.get('start', 1))
+        limit = int(request.args.get('limit', 10))
         plans = Plan.query.all()
-        return jsonify({"plans": [p.to_dict() for p in plans]})
-
+        count = len(plans)
+        if(count < start):
+            return jsonify(obj)
+        obj["start"] = start
+        obj["limit"] = limit
+        obj["count"] = 1
+        if start == 1:
+            obj["previous"] = ''
+        else:
+            start_copy = max(1, start - limit)
+            limit_copy = start - 1
+            obj["previous"] = url + '?start=%d&limit=%d' % (start_copy, limit_copy)
+        if start + limit > count:
+            obj['next'] = ''
+        else:
+            start_copy = start + limit
+            obj['next'] = url + '?start=%d&limit=%d' % (start_copy, limit)
+        plans_data = [u.to_dict() for u in plans]
+        obj["plans"] = plans_data[(start - 1): (start - 1 + limit)]
+        return jsonify(obj) 
+    elif request.method == "POST":
+        data = request.get_json()
+        plan = Plan(
+            name = data["name"],
+            payment = data["payment"],
+            card_number = data["card_number"],
+            participants_number = data["participants_number"],
+            status = data["status"],
+            participants_pay = data["participants_pay"],
+            type_pay = data["type_pay"],
+            owner_id = data["owner_id"],
+        )
+        db.session.add(plan)
+        db.session.commit()
+        return jsonify(plan.to_dict()), 201
 
 @api.route("/plan/<int:id>/", methods=["GET"])
 def plan(id):
