51b613c8cc1acee4fbf95a26d82ba3199869c609
Marco Antonio Rodriguez Torres <mrc5622@gmail.com>
2018-08-22 10:23:41 -0500

    modified config.py with new params from db connection.

--- backend/remembrallapi/config.py
+++ backend/remembrallapi/config.py
@@ -4,15 +4,13 @@ Settings for the flask application object
 
 
 class BaseConfig(object):
-    POSTGRES = {
-        "user": "postgres",
-        "pw": "",
-        "db": "remembrall",
-        "host": "localhost",
-        "port": "5432",
-    }
 
+    POSTGRES_URL = 'mydb-instance.cqak9rvjxgdz.us-east-2.rds.amazonaws.com:5432'
+    POSTGRES_USER = 'marco'
+    POSTGRES_PW = 'marcorodriguez'
+    POSTGRES_DB = 'remembrall'
+    
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s" % POSTGRES
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "i3c2xs%z4sk=9mts8)=8_u+=fng_d+gv7u$yp-zewz#kx#rn9f"
